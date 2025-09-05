3abd228f8cef5e01bb11180873a8f1096262a0ba
ecdraayer <ecdraayer@Gmail.com>
2025-09-04 22:49:08 -0600

    Add files via upload

---
+++ Experiments/Baselines/TIRE/TIRE_getcps.py
@@ -0,0 +1,288 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Lambda, Input, Dense
+from tensorflow.keras.models import Model
+
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks, peak_prominences
+import warnings
+import time, copy
+
+import utils
+import TIRE
+import simulate
+import time
+
+def _true_positives(T, X, margin=5):
+    '''
+    Compute true positives without double counting
+    Author: G.J.J. van den Burg (https://github.com/alan-turing-institute/TCPDBench)
+    Examples
+    -----------
+    >>> _true_positives({1, 10, 20, 23}, {3, 8, 20})
+    {1, 10, 20}
+    >>> _true_positives({1, 10, 20, 23}, {1, 3, 8, 20})
+    {1, 10, 20}
+    >>> _true_positives({1, 10, 20, 23}, {1, 3, 5, 8, 20})
+    {1, 10, 20}
+    >>> _true_positives(set(), {1, 2, 3})
+    set()
+    >>> _true_positives({1, 2, 3}, set())
+    set()
+    '''
+    # make a copy so we don't affect the caller
+    X = set(list(X))
+    TP = set()
+    for tau in T:
+        close = [(abs(tau - x), x) for x in X if abs(tau - x) <= margin]
+        close.sort()
+        if not close:
+            continue
+        dist, xstar = close[0]
+        TP.add(tau)
+        X.remove(xstar)
+    return TP
+
+
+def f_measure(ground_truth, predictions, margin=5, alpha=0.5, return_PR=False):
+    '''
+    Compute the F-measure based on human annotations. Remember that all CP locations are 0-based!
+    Author: G.J.J. van den Burg (https://github.com/alan-turing-institute/TCPDBench)
+    Parameters
+    -----------
+    :param annotations: dict from user_id to iterable of CP locations
+    :param predictions: iterable of predicted CP locations
+    :param alpha: value for the F-measure, alpha=0.5 gives the F1-measure
+    :return: whether to return precision and recall too
+    Examples
+    -----------
+    >>> f_measure({1: [10, 20], 2: [11, 20], 3: [10], 4: [0, 5]}, [10, 20])
+    1.0
+    >>> f_measure({1: [], 2: [10], 3: [50]}, [10])
+    0.9090909090909091
+    >>> f_measure({1: [], 2: [10], 3: [50]}, [])
+    0.8
+    '''
+    annotations =	{'1':ground_truth}
+    # ensure 0 is in all the sets
+    Tks = {k + 1: set(annotations[uid]) for k, uid in enumerate(annotations)}
+    for Tk in Tks.values():
+        Tk.add(0)
+
+    X = set(predictions)
+    X.add(0)
+
+    Tstar = set()
+    for Tk in Tks.values():
+        for tau in Tk:
+            Tstar.add(tau)
+
+    K = len(Tks)
+
+    P = len(_true_positives(Tstar, X, margin=margin)) / len(X)
+
+    TPk = {k: _true_positives(Tks[k], X, margin=margin) for k in Tks}
+    R = 1 / K * sum(len(TPk[k]) / len(Tks[k]) for k in Tks)
+
+    F = P * R / (alpha * R + (1 - alpha) * P)
+    if return_PR:
+        return F, P, R
+    return F
+
+
+def _overlap(A, B):
+    '''
+    Return the overlap (i.e. Jaccard index) of two sets
+    Author: G.J.J. van den Burg (https://github.com/alan-turing-institute/TCPDBench)
+    Examples
+    -----------
+    >>> _overlap({1, 2, 3}, set())
+    0.0
+    >>> _overlap({1, 2, 3}, {2, 5})
+    0.25
+    >>> _overlap(set(), {1, 2, 3})
+    0.0
+    >>> _overlap({1, 2, 3}, {1, 2, 3})
+    1.0
+    '''
+    return len(A.intersection(B)) / len(A.union(B))
+
+
+def _partition_from_cps(locations, n_obs):
+    '''
+    Return a list of sets that give a partition of the set [0, T-1], as
+    defined by the change point locations.
+    Author: G.J.J. van den Burg (https://github.com/alan-turing-institute/TCPDBench)
+    Examples
+    -----------
+    >>> _partition_from_cps([], 5)
+    [{0, 1, 2, 3, 4}]
+    >>> _partition_from_cps([3, 5], 8)
+    [{0, 1, 2}, {3, 4}, {5, 6, 7}]
+    >>> _partition_from_cps([1,2,7], 8)
+    [{0}, {1}, {2, 3, 4, 5, 6}, {7}]
+    >>> _partition_from_cps([0, 4], 6)
+    [{0, 1, 2, 3}, {4, 5}]
+    '''
+    T = n_obs
+    partition = []
+    current = set()
+
+    all_cps = iter(sorted(set(locations)))
+    cp = next(all_cps, None)
+    for i in range(T):
+        if i == cp:
+            if current:
+                partition.append(current)
+            current = set()
+            cp = next(all_cps, None)
+        current.add(i)
+    partition.append(current)
+    return partition
+
+
+def _cover_single(Sprime, S):
+    '''
+    Compute the covering of a segmentation S by a segmentation Sprime.
+    This follows equation (8) in Arbaleaz, 2010.
+    Author: G.J.J. van den Burg (https://github.com/alan-turing-institute/TCPDBench)
+    Examples
+    -----------
+    >>> _cover_single([{1, 2, 3}, {4, 5}, {6}], [{1, 2, 3}, {4, 5, 6}])
+    0.8333333333333334
+    >>> _cover_single([{1, 2, 3, 4}, {5, 6}], [{1, 2, 3, 4, 5, 6}])
+    0.6666666666666666
+    >>> _cover_single([{1, 2}, {3, 4}, {5, 6}], [{1, 2, 3}, {4, 5, 6}])
+    0.6666666666666666
+    >>> _cover_single([{1, 2, 3, 4, 5, 6}], [{1}, {2}, {3}, {4, 5, 6}])
+    0.3333333333333333
+    '''
+    T = sum(map(len, Sprime))
+    assert T == sum(map(len, S))
+    C = 0
+    for R in S:
+        C += len(R) * max(_overlap(R, Rprime) for Rprime in Sprime)
+    C /= T
+    return C
+
+
+def covering(ground_truth, predictions, n_obs):
+    '''
+    Compute the average segmentation covering against the human annotations.
+    Author: G.J.J. van den Burg (https://github.com/alan-turing-institute/TCPDBench)
+    Parameters
+    -----------
+    @param annotations: dict from user_id to iterable of CP locations
+    @param predictions: iterable of predicted Cp locations
+    @param n_obs: number of observations in the series
+    Examples
+    -----------
+    >>> covering({1: [10, 20], 2: [10], 3: [0, 5]}, [10, 20], 45)
+    0.7962962962962963
+    >>> covering({1: [], 2: [10], 3: [40]}, [10], 45)
+    0.7954144620811286
+    >>> covering({1: [], 2: [10], 3: [40]}, [], 45)
+    0.8189300411522634
+    '''
+    annotations =	{'1':ground_truth}
+    Ak = {
+        k + 1: _partition_from_cps(annotations[uid], n_obs)
+        for k, uid in enumerate(annotations)
+    }
+    pX = _partition_from_cps(predictions, n_obs)
+
+    Cs = [_cover_single(pX, Ak[k]) for k in Ak]
+    return sum(Cs) / len(Cs)
+    
+def main():    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_filename")
+    parser.add_argument("gt_filename")
+    parser.add_argument("margin")
+    parser.add_argument("win_size")
+    parser.add_argument("cps_output_filename")
+    parser.add_argument("dist")
+
+    args = parser.parse_args()
+
+    ts_file = args.data_filename
+    gt = args.gt_filename
+    margin = args.margin
+    cps_filename = args.cps_output_filename
+    
+    # Load data
+    timeseries = np.loadtxt(ts_file, delimiter=",")
+    timeseries = timeseries.T
+
+    labels = np.loadtxt(gt, delimiter=",")
+    ground_truth = np.where(labels[:-1] != labels[1:])[0]
+
+    window_size = args.win_size
+    domain = "both" #choose from: TD (time domain), FD (frequency domain) or both
+
+    #parameters TD
+    intermediate_dim_TD=0
+    latent_dim_TD=1 #h^TD in paper
+    nr_shared_TD=1 #s^TD in paper
+    K_TD = 2 #as in paper
+    nr_ae_TD= K_TD+1 #number of parallel AEs = K+1
+    loss_weight_TD=1 #lambda_TD in paper
+
+    #parameters FD
+    intermediate_dim_FD=10
+    latent_dim_FD=1 #h^FD in paper
+    nr_shared_FD=1 #s^FD in paper
+    K_FD = 2 #as in paper
+    nr_ae_FD=K_FD+1 #number of parallel AEs = K+1
+    loss_weight_FD=1 #lambda^FD in paper
+    nfft = 30 #number of points for DFT
+    norm_mode = "timeseries" #for calculation of DFT, should the timeseries have mean zero or each window?
+    
+    change_points = []
+    disses = []
+    times = []
+    for i in range(timeseries.shape[1]):
+        data = timeseries[:,i]
+        start = time.time()
+        windows = utils.ts_to_windows(data, 0, window_size, stride=20)
+        windows = utils.minmaxscale(windows,-1,1)
+        windows_TD = windows
+        windows_FD = utils.calc_fft(windows_TD, nfft, norm_mode)
+        shared_features_TD = TIRE.train_AE(windows_TD, intermediate_dim_TD, latent_dim_TD, nr_shared_TD, nr_ae_TD, loss_weight_TD)
+        shared_features_FD = TIRE.train_AE(windows_FD, intermediate_dim_FD, latent_dim_FD, nr_shared_FD, nr_ae_FD, loss_weight_FD)
+        dissimilarities = TIRE.smoothened_dissimilarity_measures(shared_features_TD, shared_features_FD, domain, window_size)
+        end = time.time()
+        times.append(end-start)
+        disses.append(dissimilarities)
+        change_point_scores = TIRE.change_point_score(dissimilarities, window_size)
+        change_points.append(change_point_scores)
+    times = np.array(times)
+    np.savetxt(cps_filename+"_change_points.csv", change_points, delimiter=",")
+    np.savetxt(cps_filename+"_disses.csv", disses, delimiter=",")
+    data = np.loadtxt(cps_filename+"_change_points.csv", delimiter=",") 
+    
+    actual_cps = []
+    for d in change_points:
+        actual_cps.append(np.where(d > 0.5))
+    flat_change_points = []
+    for row in actual_cps:
+        for cp in row:
+            for c in cp:
+                flat_change_points.append(c)
+    flat_change_points = np.array(flat_change_points)
+    dbs = DBSCAN1D(eps=args.dist, min_samples=1)
+    labels = dbs.fit_predict(flat_change_points)
+    predictions = []
+    for k in np.unique(labels):
+        indexes = np.where(labels == k)[0]
+        predictions.append(int(np.average(flat_change_points[indexes])))
+    print('covering score:',covering(ground_truth, predictions, len(timeseries)))
+    print('margin:',margin)
+    print('f_measure score:',f_measure(ground_truth, predictions, margin=margin, alpha=0.5, return_PR=True))
+    print(len(predictions))
+    print(np.min(times))
+if __name__ == "__main__":
+    main() 
